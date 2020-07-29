15baeb2310263076283d9c934ff721c121d7d4fc
Norashin23 <NORashikin23>
2020-07-30 04:24:18 +0800

    Update TracKingController.php

--- BusinessServiceLayer/trackingController/TracKingController.php
+++ BusinessServiceLayer/trackingController/TracKingController.php
@@ -25,4 +25,31 @@ class TrackingController
         $Status->TrackID = $TrackID;
         return $Status->viewProgress();
     }
+	function viewUnaccepted()
+    {
+        $Tracking = new TrackingModel();
+        return $Tracking->viewUnaccepted();
+    }
+	
+	function viewAccepted()
+    {
+        session_start();
+        $Tracking = new TrackingModel();
+        $Tracking->R_ID = $_SESSION['Runner_ID'];
+        return $Tracking->viewAccepted();
+    }
+	
+	function runAccept()
+    {
+        $Tracking = new TrackingModel();
+        $Tracking->TrackID = $_POST['TrackID'];
+        $Tracking->RunStatus = $_POST['RunStatus'];
+        $Tracking->R_ID = $_SESSION['Runner_ID'];
+
+        if ($Tracking->runAccept()) {
+            $message = "Success Update!";
+            echo "<script type='text/javascript'>alert('$message');
+        window.location = '../trackingView/RunJobList.php?TrackID=" . $_POST['TrackID'] . "';</script>";
+        }
+    }
 }
