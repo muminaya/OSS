499da348c9902bdd653905a61f9fc1212c768c8f
Norashin23 <NORashikin23>
2020-07-31 01:41:48 +0800

    Update TracKingController.php

--- BusinessServiceLayer/trackingController/TracKingController.php
+++ BusinessServiceLayer/trackingController/TracKingController.php
@@ -52,4 +52,42 @@ class TrackingController
         window.location = '../trackingView/RunJobList.php?TrackID=" . $_POST['TrackID'] . "';</script>";
         }
     }
+	
+	function viewStatus($TrackID)
+    {
+        $Tracking = new TrackingModel();
+        $Tracking->TrackID = $TrackID;
+        return $Tracking->viewStatus();
+    }
+
+    function updateProgress($TrackID)
+    {
+        $Status = new TrackingModel();
+        $Status->TrackID = $_POST['TrackID'];
+        $Status->StatID = $_POST['StatID'];
+        $Status->TrackDate = $_POST['TrackDate'];
+        $Status->TrackProcess = $_POST['TrackProcess'];
+        $Status->TrackTime = $_POST['TrackTime'];
+        if ($Status->updateProgress()) {
+            $message = "Success Update!";
+            echo "<script type='text/javascript'>alert('$message');
+        window.location = '../trackingView/RunUpdate.php?TrackID=" . $_POST['TrackID'] . "';</script>";
+        }
+    }
+	
+	function updateDeliveryStatus()
+    {
+        $Status = new TrackingModel();
+        $Status->TrackID = $_POST['received'];
+        $Status->updateDeliveryStatus();
+        echo "<script type='text/javascript'>alert('Thanks for using SMS Sdn Bhd');
+        window.location = '../trackingView/CusTrackList.php';</script>";
+    }
+
+    function viewRunner($TrackID)
+    {
+        $Status = new TrackingModel();
+        $Status->TrackID = $TrackID;
+        return $Status->viewRunner();
+    }
 }
