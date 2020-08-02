2ca34907f6855e811b8f64e7c63515effa7c64d3
Norashin23 <NORashikin23>
2020-08-02 21:41:44 +0800

    Update TracKingModel.php

--- BusinessServiceLayer/trackingModel/TracKingModel.php
+++ BusinessServiceLayer/trackingModel/TracKingModel.php
@@ -7,7 +7,12 @@ class TrackingModel
     public
         $TrackID, $Cus_ID, $Ord_ID, $sp_id, $CusStatus, $RunStatus, $Ship_Add, $R_ID;
 
-    
+    function viewOnD()
+    {
+        $sql = "select Service.S_Name, Tracking.Ship_Add, Tracking.TrackID from Tracking inner join order1 on Tracking.Ord_ID=order1.Ord_ID inner join service on Order1.Service_ID=Service.Service_ID where Tracking.CusStatus LIKE 'On Delivery' and Tracking.Cus_ID=:cus_ID and order1.Cus_ID=:cus_ID ";
+        $args = [':cus_ID' => $this->Cus_ID];
+        return DB::run($sql, $args);
+    }
 	
 	function viewComp()
     {
